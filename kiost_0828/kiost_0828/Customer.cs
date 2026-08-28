80b56cc1370811b868f154b7f9da64d77cbceed4
woghks62 <p2pee@naver.com>
2026-08-28 10:30:09 +0900

    [생성] Customer 클래스 생성

---
+++ kiost_0828/kiost_0828/Customer.cs
@@ -0,0 +1,22 @@
+namespace kiost_0828;
+
+public class Customer
+{
+   public int Id { get; set; }
+   public int count { get; set; }
+
+   public void Clean()
+   {
+      
+   }
+
+   public void Pay()
+   {
+      
+   }
+
+   public void End()
+   {
+      
+   }
+}
